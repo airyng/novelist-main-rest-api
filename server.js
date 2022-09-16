5ee26c9e46acc4368d170f57e6f8fdc2aec55eef
airyng <airyng@yandex.ru>
2022-09-17 00:48:19 +0300

    fixed(cors): added setting to resolve cors issue

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const router = require('./router')
+const corsMiddleware = require('./middlewares/corsMiddleware')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -11,6 +12,8 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+
+app.use(corsMiddleware)
 
 app.use('/', router)
 
