59f0b86638a9a67a67be7c7c2507ce2a95f67cb9
airyng <airyng@yandex.ru>
2022-10-09 00:54:48 +0300

    feat(game controller): enhanced getItem method

--- controllers/game.js
+++ controllers/game.js
@@ -1,4 +1,55 @@
 const DefaultController = require('./classes/Default')
 const Game = require('../models/game')
 
-module.exports = new DefaultController(Game)
+
+class GameController extends DefaultController {
+
+  constructor (model) {
+    super(model)
+    this.middlewaresRelations.getItem = []
+  }
+
+  /**
+   * Обработчик запроса возвращающий запись по переданному идентификатору
+   * @param {Object} req Объект запроса
+   * @param {Object} res Объект ответа
+   */
+  async getItem (req, res) {
+    if (!req.params.id) {
+      res.status(400)
+      return
+    }
+
+    let item
+    try {
+      item = await this.model
+              .findById(req.params.id)
+              .populate('author', ['name', 'avatar_id'])
+      
+      if (item == null) {
+        return res.status(404).json({ message: `Cannot find ${this.model.name} by given id` })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+    res.json(item)
+  }
+
+  /**
+   * Обработчик запроса возвращающий все записи
+   * @param {Object} req Объект запроса
+   * @param {Object} res Объект ответа
+   */
+  async getItems (req, res) {
+    try {
+      const items = await this.model
+                      .find()
+                      .populate('author', ['name', 'avatar_id'])
+      res.json(items)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+}
+
+module.exports = new GameController(Game)
