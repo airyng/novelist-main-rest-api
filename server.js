7886db63d77e893f3ebeb5c4df71f6719082a04f
airyng <airyng@yandex.ru>
2022-10-29 18:51:03 +0300

    feat(server): added new prop to init options to avoid deprecation warnings.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express'),
       corsMiddleware = require('./middlewares/corsMiddleware'),
       port = process.env.PORT || 3000
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
