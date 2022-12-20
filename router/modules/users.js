eb1bc389ac0e96db6a415ad29a87ad33bb45ffad
airyng <airyng@yandex.ru>
2022-12-20 18:28:55 +0300

    fix(router): returned back previous profile url

--- router/modules/users.js
+++ router/modules/users.js
@@ -3,16 +3,16 @@ const User = require.main.require('./models/user')
 
 module.exports = function (router, middlewares) {
 
-  // Getting all users
-  router.get('/users', userController.endpoint('getItems'))
-
   // Getting logged user data
   router.get(
-    '/users/profile',
+    '/profile',
     middlewares.authenticateToken,
     userController.endpoint('getProfile')
   )
   
+  // Getting all users
+  router.get('/users', userController.endpoint('getItems'))
+
   // Getting One user
   router.get('/users/:id', userController.endpoint('getItem'))
 
